6b7bec2d5113df73ec79938065b162e35309feee
abhi-chalein <69003719+abhi-chalein@users.noreply.github.com>
2020-08-22 15:43:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+doucument.querySelector(".button3").addEventListener("click",functionto);
+
+function functionto(){
+   var ans= prompt("Type in your password?");
+    alert("You have successfully signed in"+ans+" .");
+}
